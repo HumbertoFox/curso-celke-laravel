70cb4913b3934768c8a32d0dfcd30d36c1385d6b
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-11-07 13:53:46 -0300

    Add button useState

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
+"use client";
+
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Menu from "./components/Menu";
 
 function Home() {
+  const [name, setName] = useState("Acessar");
+
   return (
     <div className="text-center">
       <Menu />
 
-      Projeto Celke
+      <p>Projeto Celke</p>
+
+      <button onClick={() => setName("Humberto")}>{name}</button>
 
       <Footer />
     </div>
